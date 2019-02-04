88123e9fffaac1f6c2cd4ee331137d38afffa00b
Jason Freeberg <jr.freeberg@comcast.net>
2019-02-04 11:11:52 -0800

    added files

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys, getopt
+
+def main(argv):
+   inputdir = ''
+   outputdir = ''
+
+   print("Input directory: " + inputdir)
+   print("Output directory: " + outputdir)
+
+   """ 
+   For each file:
+
+        Get the date from the <time> 
+        Copy to new directory with the date prepended
+        Add the slash to anything following "src=", "href=", 
+        
+   """
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
