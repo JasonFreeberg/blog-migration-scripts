4d527d46cc15b157c2ab63152147d32154b2ea43
Jason Freeberg <jr.freeberg@comcast.net>
2019-02-08 11:37:47 -0800

    version 2

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# blog-migration-scripts
+# MSDN Migration Tools
+
+A collection of Python methods to migrate App Service MSDN blog content to GitHub Pages.
+
+Applies the following fixes:
+- Prepend the YYYY-MM-DD to file names
+- Edit the file paths for all images and JS
+- Remove CSS, as Jekyll applies its own
+- Remove the \<h1> title tag, as Jekyll adds that for us
+
+## Usage
+
+You must activate the virtual environment and import the dependencies in `requirements.txt`.
+
+Powershell:
+```
+cd blog-migration-scripts
+.\venv\Scripts\activate
+pip3 install -r requirements.txt
+```
+
+Finally, run the main command to edit the MSDN HTML files:
+
+```
+python main.py <input dir> <output dir>
+```
+
+Where `input dir` is the directory with the MSDN blogs and `output dir` is where you would like the edited files to be saved. This tool does **not** edit the files in the input directory.
--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-import sys, getopt
+
+import sys
+import os
+import shutil
+from migration import helpers
+
 
 def main(argv):
-   inputdir = ''
-   outputdir = ''
+    input_dir = argv[0]
+    output_dir = argv[1]
 
-   print("Input directory: " + inputdir)
-   print("Output directory: " + outputdir)
+    print("Input directory: " + input_dir)
+    print("Output directory: " + output_dir)
 
-   """ 
-   For each file:
+    helpers.copy_files(input_dir, output_dir)
+    print("Copied files to output directory.")
 
-        Get the date from the <time> 
-        Copy to new directory with the date prepended
-        Add the slash to anything following "src=", "href=", 
-        
-   """
+    try:
+        for f in helpers.get_html_files(output_dir):
+            print("Editing file: " + os.path.basename(f))
+            helpers.edit_src_and_href_paths(f)
+            helpers.add_header_markup(f)
+            helpers.remove_title_tag(f)
+            helpers.update_file_name(f) # Update the file name last
+    except Exception as e:
+        print(e)
+        print('Batch process failed. Deleting the contents of the output directory.')
+        for filename in os.listdir(output_dir):
+            filepath = os.path.join(output_dir, filename)
+            try:
+                shutil.rmtree(filepath)
+            except OSError:
+                os.remove(filepath)
+
+    print('Done.')
+    exit()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
